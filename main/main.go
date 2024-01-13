dffcf472604b7c65bb21a913401ffc00c1eb18da
liweijie_ubuntu <helloworld.liweili@gmail.com>
2024-01-13 23:36:23 +0800

    up

--- main/main.go
+++ main/main.go
@@ -1,7 +1,8 @@
 package main
 
-import "github.com/liweilicoder/LearnGolang/ch1"
+import "github.com/liweilicoder/LearnGolang/ch2"
 
 func main() {
-	ch1.Server4()
+	//ch1.Server4()
+	ch2.Echo4()
 }
