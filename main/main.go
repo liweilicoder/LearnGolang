ee14c6b2fa71f759ce7a85b0c12a783d2b49f753
liweijie_ubuntu <helloworld.liweili@gmail.com>
2024-01-15 21:58:07 +0800

    chapter2 done!

--- main/main.go
+++ main/main.go
@@ -2,22 +2,24 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"strconv"
 
 	"github.com/liweilicoder/LearnGolang/ch2"
 )
 
 func main() {
-	//ch1.Server4()
-	for _, arg := range os.Args[1:] {
-		t, err := strconv.ParseFloat(arg, 64)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "cf:%v\n", err)
-			os.Exit(1)
-		}
-		f := ch2.Fahrenheit(t)
-		c := ch2.Celsius(t)
-		fmt.Printf("%s = %s, %s = %s\n", f, ch2.FToC(f), c, ch2.CToF(c))
-	}
+	// //ch1.Server4()
+	// for _, arg := range os.Args[1:] {
+	// 	t, err := strconv.ParseFloat(arg, 64)
+	// 	if err != nil {
+	// 		fmt.Fprintf(os.Stderr, "cf:%v\n", err)
+	// 		os.Exit(1)
+	// 	}
+	// 	f := ch2.Fahrenheit(t)
+	// 	c := ch2.Celsius(t)
+	// 	fmt.Printf("%s = %s, %s = %s\n", f, ch2.FToC(f), c, ch2.CToF(c))
+	// }
+
+	var x uint64
+	fmt.Scan(&x)
+	fmt.Println(ch2.Popcount(x))
 }
